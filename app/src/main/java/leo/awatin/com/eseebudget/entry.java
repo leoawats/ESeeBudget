dfacfc654b677d9172d0d2f38e340f455e9940f9
leoawats <2015081427@ust-ics.mygbiz.com>
2018-11-15 08:59:46 +0800

    Transitions between activities

--- app/src/main/java/leo/awatin/com/eseebudget/entry.java
+++ app/src/main/java/leo/awatin/com/eseebudget/entry.java
@@ -2,6 +2,8 @@ package leo.awatin.com.eseebudget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class entry extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class entry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_entry );
+    }
+
+    public void toIndex(View v) {
+        Intent i = new Intent(this, index.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/leo/awatin/com/eseebudget/index.java
+++ app/src/main/java/leo/awatin/com/eseebudget/index.java
@@ -2,6 +2,8 @@ package leo.awatin.com.eseebudget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class index extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class index extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_index );
+    }
+
+    public void toEntry(View v) {
+        Intent i = new Intent(this, entry.class);
+        startActivity(i);
     }
 }
